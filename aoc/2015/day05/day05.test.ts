296286a851ed69b17638bc46b3ca3e3af944ecb4
Nico Baier <nico.baier@gmail.com>
2024-12-15 15:43:46 -0600

    feat: add day 5, 2015

---
+++ aoc/2015/day05/day05.test.ts
@@ -0,0 +1,30 @@
+import path from "node:path";
+import type { TestData, TestMap } from "@/types";
+import { getDayPath, loadFile } from "@/utils";
+import { createTestMap, createTestSet } from "@/utils/test";
+import { describe } from "vitest";
+import solution from ".";
+
+const year = "2015";
+const day = "05";
+const dayPath = getDayPath(year, day);
+
+const examples1: TestData[] = [
+	{ input: "ugknbfddgicrmopn", output: 1 },
+	{ input: "aaa", output: 1 },
+	{ input: "jchzalrnumimnmhp", output: 0 },
+	{ input: "haegwjzuvuyypxyu", output: 0 },
+	{ input: "dvszwmarrgswjxmb", output: 0 },
+];
+const examples2: TestData[] = [{ input: "", output: 0 }];
+
+const testMap1: TestMap = createTestMap(examples1);
+const testMap2: TestMap = createTestMap(examples2);
+
+describe("day 05 part 1", () => {
+	createTestSet(testMap1, solution, "p1");
+});
+
+describe("day 05 part 2", () => {
+	createTestSet(testMap2, solution, "p2");
+});
