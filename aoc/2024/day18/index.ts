3a25c83c2cf85188ec8e074867b9f7df757aa2b2
Nico Baier <nico.baier@gmail.com>
2024-12-18 22:47:24 -0600

    feat: init day 1 2024

---
+++ aoc/2024/day18/index.ts
@@ -0,0 +1,13 @@
+import { getRunMode } from "@/utils";
+
+const runMode = getRunMode();
+
+function part1(input: string): number | string {
+	return 0;
+}
+
+function part2(input: string): number | string {
+	return 0;
+}
+
+export default { p1: part1, p2: part2 };
