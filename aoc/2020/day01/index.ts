a8e6c1ff541ebfacb95b15eb4513147acfb462ad
Nico Baier <nico.baier@gmail.com>
2024-12-18 22:46:25 -0600

    feat: add day 1 part 1 2020

---
+++ aoc/2020/day01/index.ts
@@ -0,0 +1,28 @@
+import { getRunMode } from "@/utils";
+
+const runMode = getRunMode();
+
+function parse(input: string) {
+	return input.trim().split("\n").map(Number);
+}
+
+function part1(input: string): number | string {
+	const arr = parse(input);
+	const target = 2020;
+	const answer: number[] = [];
+	for (const n of arr) {
+		const diff = target - n;
+		if (arr.includes(diff)) {
+			answer.push(n);
+			answer.push(diff);
+			break;
+		}
+	}
+	return answer.reduce((a, c) => a * c, 1);
+}
+
+function part2(input: string): number | string {
+	return 0;
+}
+
+export default { p1: part1, p2: part2 };
